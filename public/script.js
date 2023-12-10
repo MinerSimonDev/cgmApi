1bbf61a8f36404a8d5070abaaf9f5bd975f821b5
MinerSimonDev <simon8502x@gmail.com>
2023-12-10 18:29:55 +0100

    rebuild setup commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,102 @@
-console.log("script loaded");
+const bs = document.getElementById('bloodSugarValue');
+const ago = document.getElementById('time');
+const date = document.getElementById('date');
+const delta = document.getElementById('delta');
+const alarm = document.getElementById('alarm');
+
+function setInitialValue() {
+    if (!localStorage.getItem('lastValue')) {
+        localStorage.setItem('lastValue', new Date().getTime().toString());
+    }
+}
+
+function updateAgo() {
+    setInterval(function () {
+        var currentDate = new Date();
+        var lastValue = localStorage.getItem('lastValue');
+
+        if (lastValue) {
+            var differenceInMinutes = Math.floor((currentDate - new Date(parseInt(lastValue))) / (1000 * 60));
+
+            ago.textContent = differenceInMinutes + " Min";
+        } else {
+            ago.textContent = "0 Min";
+        }
+    }, 60000);
+}
+
+setInitialValue();
+updateAgo();
+
+function dateToBetterDate(date) {
+    var day = ("0" + date.getDate()).slice(-2);
+    var month = ("0" + (date.getMonth() + 1)).slice(-2);
+    var year = date.getFullYear();
+    var hours = ("0" + date.getHours()).slice(-2);
+    var minutes = ("0" + date.getMinutes()).slice(-2);
+
+    var formattedDate = day + ". " + month + ". " + year;
+    var formattedTime = hours + ":" + minutes;
+
+    return formattedDate + " | " + formattedTime;
+}
+
+async function fetchCurrentBloodSugar() {
+    try {
+        const response = await fetch('/api/v1/currentBloodSugar');
+        const data = await response.json();
+
+        if (data.currentBloodSugar !== undefined) {
+            var newDate = new Date(data.dateString);
+            console.log(newDate);
+
+            var currentDate = new Date();
+            var lastValue = localStorage.getItem('lastValue');
+            var differenceInMinutes = Math.floor((currentDate - newDate) / (1000 * 60));
+
+            ago.textContent = differenceInMinutes + " Min";
+
+            localStorage.setItem("lastValue", currentDate.getTime());
+            bs.textContent = data.currentBloodSugar;
+            date.textContent = dateToBetterDate(newDate);
+            delta.textContent = data.delta.toFixed(1);
+            switch (true) {
+                case data.currentBloodSugar > 180:
+                    alarm.textContent = "High";
+                    alarm.style.color = 'rgb(255, 230, 0)';
+                    break;
+
+                case data.currentBloodSugar > 230:
+                    alarm.textContent = "Very high";
+                    alarm.style.color = 'rgb(255, 106, 0)';
+                    break;
+
+                case data.currentBloodSugar < 180:
+                    alarm.textContent = "None";
+                    alarm.style.color = 'rgb(60, 227, 60)';
+                    break;
+
+                case data.currentBloodSugar < 80:
+                    alarm.textContent = "Low";
+                    alarm.style.color = 'rgb(255, 230, 0)';
+                    break;
+
+                case data.currentBloodSugar < 60:
+                    alarm.textContent = "Very low";
+                    alarm.style.color = 'rgb(255, 106, 0)';
+                    break;
+
+                default:
+                    alarm.textContent = 'None';
+                    break;
+            }
+        } else {
+            console.error('Blutzuckerwert nicht verfügbar');
+        }
+    } catch (error) {
+        console.error('Fehler beim Abrufen des Blutzuckerwerts:', error);
+    }
+}
+
+fetchCurrentBloodSugar();
+setInterval(fetchCurrentBloodSugar, 60000);  // Change the interval to 60000 milliseconds (1 minute)
--- server.js
+++ server.js
@@ -1,27 +1,73 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-const glucoseData = [];
+let currentBloodSugarData = null;
 
-app.post('/api/v1', (req, res) => {
-  const newData = req.body;
-  glucoseData.push(newData);
-  res.status(201).json(newData);
-});
-
-app.get('/api/v1', (req, res) => {
-  res.json(glucoseData);
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
 });
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.post('/api/v1/entries', (req, res) => {
+  const entryData = req.body;
+
+  if (Array.isArray(entryData) && entryData.length > 0) {
+    const now = new Date();
+    const currentEntry = entryData.find(entry => {
+      const entryDate = new Date(entry.sysTime);
+      return (
+        entryDate.getFullYear() === now.getFullYear() &&
+        entryDate.getMonth() === now.getMonth() &&
+        entryDate.getDate() === now.getDate() &&
+        entryDate.getHours() === now.getHours() &&
+        entryDate.getMinutes() === now.getMinutes()
+      );
+    });
+
+    console.log('Current entry:', currentEntry);
+
+    if (currentEntry) {
+      currentBloodSugarData = {
+        currentBloodSugar: currentEntry.sgv,
+        delta: currentEntry.delta,
+        sgv: currentEntry.sgv,
+        dateString: currentEntry.dateString
+      };
+
+      res.json(currentBloodSugarData);
+    } else {
+      res.status(404).json({ error: 'Blutzuckerwert nicht verfügbar' });
+    }
+  } else {
+    res.status(400).json({ error: 'Ungültige Eingabe' });
+  }
 });
+
+app.get('/api/v1/currentBloodSugar', (req, res) => {
+  if (currentBloodSugarData !== null) {
+    res.json(currentBloodSugarData);
+  } else {
+    res.status(404).json({ error: 'Blutzuckerwert nicht verfügbar' });
+  }
+});
+
+app.get('/api/v1/currentBloodSugar/raw', (req, res) => {
+  if (currentBloodSugarData !== null) {
+    res.json(currentBloodSugarData);
+  } else {
+    res.status(404).json({ error: 'Blutzuckerwert nicht verfügbar' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
